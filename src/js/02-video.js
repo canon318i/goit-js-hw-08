5fe5455e965d3f2952b1d18fbd4ad0166e717a6f
Oleksii Kononenko <canon318i@gmail.com>
2022-01-31 01:05:00 +0200

    Another test for wrong string

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,18 +1,18 @@
 import throttle from 'lodash/throttle';
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
-// const initialTime = localStorage.getItem('videoplayer-current-time') ?? 0;
+const initialTime = localStorage.getItem('videoplayer-current-time') ?? 0;
 
-player.setCurrentTime(initialTime);
+// player.setCurrentTime(initialTime);
 
-player.on(
-  'timeupdate',
-  throttle(() => {
-    player.getCurrentTime().then(seconds => {
-      localStorage.setItem('videoplayer-current-time', Math.round(seconds));
-    });
-  }, 1000),
-);
+// player.on(
+//   'timeupdate',
+//   throttle(() => {
+//     player.getCurrentTime().then(seconds => {
+//       localStorage.setItem('videoplayer-current-time', Math.round(seconds));
+//     });
+//   }, 1000),
+// );
 
 //Вариант с выносом getCurrentTime() в отдельную функцию
 // player.on('timeupdate', throttle(saveTime, 1000));
