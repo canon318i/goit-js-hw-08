7205b0fd8d92a034f4cbae99002691ccd04bd8b7
Oleksii Kononenko <canon318i@gmail.com>
2022-01-30 19:15:07 +0200

    First task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,20 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryMarkup = createGalleryPalette(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay:250});
+
+function createGalleryPalette(galleryItems)  {
+    return galleryItems.map(({ preview, original, description }) => {return `
+        <a class="gallery__item" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" /></a>
+        `}).join('');
+};
