4528c551da05638b07d62cbf51405451f92898bd
Oleksii Kononenko <canon318i@gmail.com>
2022-02-19 13:29:34 +0200

    Removed unused variable in  Task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,7 +2,7 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-import SimpleLightbox from "simplelightbox";
+import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // console.log(galleryItems);
@@ -11,11 +11,15 @@ const galleryMarkup = createGalleryPalette(galleryItems);
 const galleryRef = document.querySelector('.gallery');
 galleryRef.insertAdjacentHTML('afterbegin', galleryMarkup);
 
-var lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay:250});
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
 
-function createGalleryPalette(galleryItems)  {
-    return galleryItems.map(({ preview, original, description }) => {return `
+function createGalleryPalette(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
         <a class="gallery__item" href="${original}">
         <img class="gallery__image" src="${preview}" alt="${description}" /></a>
-        `}).join('');
-};
+        `;
+    })
+    .join('');
+}
