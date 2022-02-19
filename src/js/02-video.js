842983f4a13f25d0461770954ad36be978c1c316
Oleksii Kononenko <canon318i@gmail.com>
2022-02-19 13:40:11 +0200

    Added CONST for storage item name

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,12 @@
 import throttle from 'lodash/throttle';
+const CURRENT_TIME_ITEM_NAME = 'videoplayer-current-time';
 const vimeoPlayerRef = document.querySelector('#vimeo-player');
 const player = new Vimeo.Player(vimeoPlayerRef);
 //the follownig code was replaced to IF due to strange build error:
 // /home/runner/work/goit-js-hw-08/goit-js-hw-08/src/js/03-feedback.js: Unexpected token: operator (?)
 //
-// const initialTime = localStorage.getItem('videoplayer-current-time') ?? 0;
-let initialTime = localStorage.getItem('videoplayer-current-time');
+// const initialTime = localStorage.getItem(CURRENT_TIME_ITEM_NAME) ?? 0;
+let initialTime = localStorage.getItem(CURRENT_TIME_ITEM_NAME);
 if (!initialTime) {
   initialTime = 0;
 }
@@ -16,7 +17,7 @@ player.on(
   'timeupdate',
   throttle(() => {
     player.getCurrentTime().then(seconds => {
-      localStorage.setItem('videoplayer-current-time', Math.round(seconds));
+      localStorage.setItem(CURRENT_TIME_ITEM_NAME, Math.round(seconds));
     });
   }, 1000),
 );
@@ -26,6 +27,6 @@ player.on(
 
 // function saveTime() {
 //   player.getCurrentTime().then(seconds => {
-//     localStorage.setItem('videoplayer-current-time', Math.round(seconds));
+//     localStorage.setItem(CURRENT_TIME_ITEM_NAME, Math.round(seconds));
 //   });
 // }
