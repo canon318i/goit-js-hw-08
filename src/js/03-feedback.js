bdcd4204a670190798ec6256fc0514a53d2babda
Oleksii Kononenko <canon318i@gmail.com>
2022-02-19 14:43:56 +0200

    Join to setFieldsFromStorage
    Join setFormFields() and getFieldsFromStorage() to setFieldsFromStorage()

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -2,27 +2,13 @@ import throttle from 'lodash/throttle';
 const LOCAL_STORAGE_ITEM_NAME = 'feedback-form-state';
 const formRef = document.querySelector('.feedback-form');
 
-setFormFields(getFieldsFromStorage());
-
+// setFormFields(getFieldsFromStorage());
+setFieldsFromStorage();
 formRef.addEventListener('input', throttle(onFormInput, 1000));
 formRef.addEventListener('submit', onSubmit);
 
-function setFormFields({ email, message }) {
-  formRef.elements.email.value = email;
-  formRef.elements.message.value = message;
-}
-
-function getFieldsFromStorage() {
+function setFieldsFromStorage() {
   try {
-    //the follownig code was replaced to IF due to strange build error:
-    // /home/runner/work/goit-js-hw-08/goit-js-hw-08/src/js/03-feedback.js: Unexpected token: operator (?)
-    //
-    // return (
-    //   JSON.parse(localStorage.getItem(LOCAL_STORAGE_ITEM_NAME)) ?? {
-    //     email: '',
-    //     message: '',
-    //   }
-    // );
     let result = JSON.parse(localStorage.getItem(LOCAL_STORAGE_ITEM_NAME));
     if (!result) {
       result = {
@@ -30,12 +16,46 @@ function getFieldsFromStorage() {
         message: '',
       };
     }
-    return result;
+    formRef.elements.email.value = result.email;
+    formRef.elements.message.value = result.message;
+    return;
   } catch (error) {
     console.log('Set state error: ', error.message);
-    return { email: '', message: '' };
+    formRef.elements.email.value = '';
+    formRef.elements.message.value = '';
+    return;
   }
 }
+
+// function setFormFields({ email, message }) {
+//   formRef.elements.email.value = email;
+//   formRef.elements.message.value = message;
+// }
+
+// function getFieldsFromStorage() {
+//   try {
+//     //the follownig code was replaced to IF due to strange build error:
+//     // /home/runner/work/goit-js-hw-08/goit-js-hw-08/src/js/03-feedback.js: Unexpected token: operator (?)
+//     //
+//     // return (
+//     //   JSON.parse(localStorage.getItem(LOCAL_STORAGE_ITEM_NAME)) ?? {
+//     //     email: '',
+//     //     message: '',
+//     //   }
+//     // );
+//     let result = JSON.parse(localStorage.getItem(LOCAL_STORAGE_ITEM_NAME));
+//     if (!result) {
+//       result = {
+//         email: '',
+//         message: '',
+//       };
+//     }
+//     return result;
+//   } catch (error) {
+//     console.log('Set state error: ', error.message);
+//     return { email: '', message: '' };
+//   }
+// }
 
 function onFormInput() {
   localStorage.setItem(
